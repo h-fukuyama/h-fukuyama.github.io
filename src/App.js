c220e38d5ba210e0dfe277ad07d0877be34eb171
h-fukuyama <h-fukuyama@usen-next.jp>
2024-05-28 12:39:15 +0900

    App周りとヘッダーの不要な部分削除、css見直し

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-// App.js
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { FileProvider } from './fileOperation/FileContext';
 import OthrComponent from './components/mainpage/SettingPage';
 import MainComponent from './components/mainpage/HomePage';
 import IsmsComponent from './components/mainpage/ChannelMaskPage';
@@ -9,7 +6,8 @@ import LtPage from './components/mainpage/LtPage.js';
 import LtDetail from './components/lt/LtDetail';
 import FileInputScreen from './components/startpage/FileInputScreen';
 import ResetComponent from './components/general/ResetComponent.js';
-import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { FileProvider } from './fileOperation/FileContext';
 
 function App() {
   return (
--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 
+const headerStyle = {
+    position: "sticky",
+    top: "0",
+    width: "100%",
+    backgroundColor: "black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    fontSize: "15px",
+    color: "white",
+    zIndex: "100"
+}
+
 const Header = () => {
     return (
-        <div id="header">
+        <div style={headerStyle}>
             <nav>
                 <ul style={{ display: 'flex', alignItems: 'center', margin: '10px' }}>
                     <li><Link to="/main">ホーム</Link></li>
-                    {/* <li><Link to="/sc">不要！<br />スタッフコール</Link></li>
-                    <li><Link to="/menu">不要！<br />ワンタッチボタン</Link></li> */}
                     <li><Link to="/isms">チャンネルマスク</Link></li>
                     <li><Link to="/lt">ローカルタイマー</Link></li>
                     <li><Link to="/othr">設定値</Link></li>
